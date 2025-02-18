8080e5d89a14cac1c6773cc6bda9edad92a89cb6
dohaehab <dohaehab@gmail.com>
2025-02-18 23:28:32 +0200

    task 2

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 """this state file handle rest API"""
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from models import storage
 
 
-@app_views.route('/states/<state_id>', methods=['GET', 'DELETE'])
-def get_states_by_id():
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
+def get_states_by_id(state_id):
     """get state by id"""
-    storage.all('State')
-    return None
+    result = storage.get('State', state_id)
+    if result is None:
+        abort(404, 'Not found')
+
+    if request.method == 'GET':
+        return jsonify(result.to_json())
+
+    if request.method == 'DELETE':
+        result.delete()
+        del result
+        return {}, 200
+
+    if request.method == 'PUT':
+        result = request.get_json()
+        if result is None:
+            abort(400, 'Not a JSON')
 
 
 @app_views.route('/states', methods=['GET', 'POST'])
@@ -17,3 +31,10 @@ def get_states():
     """get all states"""
     if request.method == 'GET':
         return jsonify(storage.all('State').to_dict())
+
+    if request.method == 'POST':
+        result = request.get_json()
+        if result is None:
+            abort(400, 'Not a JSON')
+        if result.get("name") is None:
+            abort(400, 'Missing name')
