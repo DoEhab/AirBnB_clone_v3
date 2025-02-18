07d1efee3e7640f812b55047f064f2aba9a5669e
dohaehab <dohaehab@gmail.com>
2025-02-18 04:59:53 +0200

    task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import request, jsonify
+from flask import jsonify, request
 
 
-@app_views.route('/status', method=['GET'])
+@app_views.route('/status', methods=['GET'])
 def get_status():
     """Get method to return status"""
     if request.method == 'GET':
